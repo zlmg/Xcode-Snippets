19564e324e563f1f4d688fa33053ab9cdde67228
zlmg <zlmg4@163.com>
2016-07-14 18:04:43 +0800

    test

--- atestfoldertest.m
+++ atestfoldertest.m
@@ -22,27 +22,6 @@
     }
 }
 
-- (void)syncSelectedText:(id)sender {
-    id<ACCodeSnippetDataStoreProtocol> dataStore = [[NSClassFromString(@"IDECodeSnippetRepository") sharedRepository] dataStores].firstObject;
-    
-    NSData *textData = [self.selectedText dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *folderName = [NSString pathWithComponents:@[dataStore.localRepositoryPath, @"aTestFolder"]];
-    if (![[NSFileManager defaultManager] fileExistsAtPath:folderName]) {
-        [[NSFileManager defaultManager] createDirectoryAtPath:folderName withIntermediateDirectories:YES attributes:nil error:nil];
-    }
-    NSString *fileName = [NSString pathWithComponents:@[folderName, @"test.m"]];
-    if (![[NSFileManager defaultManager] fileExistsAtPath:fileName]) {
-        [textData writeToFile:fileName atomically:YES];
-    } else {
-        NSLog(@"file exists");
-        NSString *text = [NSString stringWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:nil];
-        text = [NSString stringWithFormat:@"%@\n\n%@", text, self.selectedText];
-        NSData *data = [text dataUsingEncoding:NSUTF8StringEncoding];
-        [data writeToFile:fileName atomically:YES];
-    }
-    [self updateAction:nil];
-}
-
 #pragma mark - NSTextView Action
 - (void)selectionDidChange:(NSNotification *)noti {
     if ([noti.object isKindOfClass:[NSTextView class]]) {
